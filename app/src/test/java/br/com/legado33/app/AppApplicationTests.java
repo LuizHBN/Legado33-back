34725e9b36ddd202c0ea3d28512ba17dd022940b
Luiz Henrique Netto <lh.netto11@gmail.com>
2024-04-03 20:13:13 -0300

    Initial Commit

---
+++ app/src/test/java/br/com/legado33/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.legado33.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
