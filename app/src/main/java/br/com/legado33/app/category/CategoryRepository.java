eac1f6169f54ce2b3e7b7e173a296447a1225399
Gustavo Belo <gustavobelo28@gmail.com>
2024-06-18 22:59:58 -0300

    Adicionar novas alterações no CategoryRepository.java

--- app/src/main/java/br/com/legado33/app/category/CategoryRepository.java
+++ app/src/main/java/br/com/legado33/app/category/CategoryRepository.java
@@ -1,7 +1,8 @@
 package br.com.legado33.app.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-//Comment by Luiz Netto
+//New Changes 
+
 
 public interface CategoryRepository extends JpaRepository<Category,Long> {
 
