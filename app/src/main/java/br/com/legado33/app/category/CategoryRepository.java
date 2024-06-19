b5da81a2bece0b617c47368f27f6bd28799513cf
Gustavo Belo <gustavobelo28@gmail.com>
2024-06-18 22:58:57 -0300

    teste luiz
    Co-authored-by: Luiz Henrique Netto <LuizHBN@users.noreply.github.com>

--- app/src/main/java/br/com/legado33/app/category/CategoryRepository.java
+++ app/src/main/java/br/com/legado33/app/category/CategoryRepository.java
@@ -1,6 +1,7 @@
 package br.com.legado33.app.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+//Comment by Luiz Netto
 
 public interface CategoryRepository extends JpaRepository<Category,Long> {
 
